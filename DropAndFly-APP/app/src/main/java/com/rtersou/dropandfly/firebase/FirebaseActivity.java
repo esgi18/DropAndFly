4f76572fd3ebbedb050e729b7555cbe9bcafb7e3
Robin Tersou <tersou.robin@gmail.com>
2019-02-19 18:07:23 +0100

    connection firebase

---
+++ DropAndFly-APP/app/src/main/java/com/rtersou/dropandfly/firebase/FirebaseActivity.java
@@ -0,0 +1,22 @@
+package com.rtersou.dropandfly.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.rtersou.dropandfly.R;
+import com.rtersou.dropandfly.firebase.dummy.DummyContent;
+
+public class FirebaseActivity extends AppCompatActivity implements FirebaseFragment.OnListFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Toast.makeText(this, item.toString(), Toast.LENGTH_SHORT).show();
+    }
+}
--- DropAndFly-APP/app/src/main/java/com/rtersou/dropandfly/registration/RegistrationActivity.java
+++ DropAndFly-APP/app/src/main/java/com/rtersou/dropandfly/registration/RegistrationActivity.java
@@ -1,13 +1,15 @@
-package com.rtersou.dropandfly;
+package com.rtersou.dropandfly.registration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.rtersou.dropandfly.R;
+
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
     }
 }
